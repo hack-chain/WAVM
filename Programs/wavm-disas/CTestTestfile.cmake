85c4bb5021424577048abc3f491884c720b13885
Dmitry Inyutin <inyutin.da@phystech.edu>
2018-11-27 01:48:26 +0300

    rm -rf wavm-disas

--- Programs/wavm-disas/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmitry/Code/WAVM/Programs/wavm-disas
-# Build directory: /home/dmitry/Code/WAVM/Programs/wavm-disas
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
