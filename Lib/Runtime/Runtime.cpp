38f77978ec0082e3271e789691852aec37d12a4c
Dmitry Inyutin <inyutin.da@phystech.edu>
2018-11-30 15:53:56 +0300

    remove context

--- Lib/Runtime/Runtime.cpp
+++ Lib/Runtime/Runtime.cpp
@@ -1,10 +1,43 @@
 #include "WAVM/Runtime/Runtime.h"
 #include "RuntimePrivate.h"
+#include "WAVM/Platform/Memory.h"
 #include "WAVM/Inline/Lock.h"
 
 using namespace WAVM;
 using namespace WAVM::IR;
 using namespace WAVM::Runtime;
+
+Context *Runtime::createContext(Compartment *compartment) {
+    wavmAssert(compartment);
+    Context *context = new Context(compartment);
+    {
+        Lock<Platform::Mutex> lock(compartment->mutex);
+
+        // Allocate an ID for the context in the compartment.
+        context->id = compartment->contexts.add(UINTPTR_MAX, context);
+        if (context->id == UINTPTR_MAX) {
+            delete context;
+            return nullptr;
+        }
+        context->runtimeData = &compartment->runtimeData->contexts[context->id];
+
+        // Commit the page(s) for the context's runtime data.
+        errorUnless(Platform::commitVirtualPages(
+                            (U8 *) context->runtimeData, sizeof(ContextRuntimeData) >> Platform::getPageSizeLog2()));
+
+        // Initialize the context's global data.
+        memcpy(context->runtimeData->mutableGlobals,
+               compartment->initialContextMutableGlobals,
+               maxGlobalBytes);
+    }
+
+    return context;
+}
+
+Runtime::Context::~Context() {
+    wavmAssertMutexIsLockedByCurrentThread(compartment->mutex);
+    compartment->contexts.removeOrFail(id);
+}
 
 #define DEFINE_OBJECT_TYPE(kindId, kindName, Type)                                                 \
     Runtime::Type* Runtime::as##kindName(Object* object)                                           \
